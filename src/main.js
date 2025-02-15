83605558114975c3c1b05cbaf2da99e75acf240a
zeynab.mvs <zeynab.mvs@gmail.com>
2025-02-15 18:38:22 +0330

    customize cursur

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const boxes = document.querySelectorAll(".bento-grid-item");
-    
-    boxes.forEach((box, index) => {
-      setTimeout(() => {
-        box.classList.add("active"); // Add active class with delay
-      }, index * 200); // Staggered delay: 200ms per box
-    });
+  const boxes = document.querySelectorAll(".bento-grid-item");
+
+  boxes.forEach((box, index) => {
+    setTimeout(() => {
+      box.classList.add("active"); // Add active class with delay
+    }, index * 200); // Staggered delay: 200ms per box
   });
-  
+});
+const cursor = document.createElement("div");
+cursor.classList.add("cursor");
+document.body.appendChild(cursor);
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
